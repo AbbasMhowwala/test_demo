6c9a98d74a73639d3cb3a1a5eb8c813239cc0388
Abbas Mhowwala <89777973+AbbasMhowwala@users.noreply.github.com>
2022-03-07 19:25:29 +0530

    Middle menu clear.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import './Responsive.css';
-import Menu from './components/Menu';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Topnav from './components/Topnav';
-import Slider from './components/Slider';
-import MobileMenu from './components/MobileMenu';
-import MiddleMenu from './components/MiddleMenu';
+import "./App.css";
+import "./Responsive.css";
+import Menu from "./components/Menu";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Topnav from "./components/Topnav";
+import Slider from "./components/Slider";
+import MobileMenu from "./components/MobileMenu";
+import MiddleMenu from "./components/MiddleMenu";
 
 function App() {
   return (
-    <div className='main_demo'>
-    <Topnav />
-    <MiddleMenu />
+    <div className="main_demo">
+      <Topnav />
+      <MiddleMenu />
       <Menu />
       <MobileMenu />
       <Slider />
--- src/components/MiddleMenu.js
+++ src/components/MiddleMenu.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Col, Container, Image, Nav, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 const MiddleMenu = () => {
   return (
-    <div className="bw-middle">
+    <div className="bw-middle desktop_menu">
       <div className="top-header">
         <Container>
-          <Row>
+          <Row className="align-items-center">
             <Col md={6} className="no-padding">
               <div className="top_left_content">
                 <ul className="list-inline-block pull-left">
@@ -19,7 +19,6 @@ const MiddleMenu = () => {
                         width="20px"
                       />
                       <span>(646) 992-9024</span>
-                      {/*                            <span class="text">Call or text 24/7</span>*/}
                     </a>
                   </li>
                   <li className="email_second">
@@ -36,11 +35,10 @@ const MiddleMenu = () => {
                 </ul>
               </div>
             </Col>
-            <Col md={6} className="no-padding float-right">
-              <div className="top_right_content desktop_view_top_head">
-                <ul className="list-inline-block pull-right">
+            <Col md={6} className="no-padding">
+              <div className="top_right_content desktop_view_top_head text-right">
+                <ul className="list-inline-block text-right">
                   <li className="login-users">
-                    <input defaultValue={0} id="login_ch" type="hidden" />{" "}
                     <a
                       href="https://demo.belgiumwebnet.com/login"
                       className="title12 white link-account"
@@ -74,35 +72,6 @@ const MiddleMenu = () => {
                         width="20px"
                       />
                       My Cart (<span id="mini-cart-number">0</span>)
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div
-                className="top_right_content mobile_view_top_head"
-                style={{ display: "none" }}
-              >
-                <ul className="list-inline-block pull-right">
-                  <li>
-                    <a href="https://demo.belgiumwebnet.com/wishlist">
-                      <img
-                        src="https://demo.belgiumwebnet.com/assets/images/gems_img/icon/heart.svg"
-                        alt="Wishlist"
-                        className="top_img"
-                        width="20px"
-                      />
-                      (<span id="wish_count_top">0</span>)
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://demo.belgiumwebnet.com/shopping/cart">
-                      <img
-                        src="https://demo.belgiumwebnet.com/assets/images/gems_img/icon/bag.svg"
-                        alt="Cart"
-                        className="top_img"
-                        width="20px"
-                      />
-                      (0)
                     </a>
                   </li>
                 </ul>
